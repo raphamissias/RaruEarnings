2c789b37cdbe54db5ee6ebe6097d8ef9130985be
Raphael <raphaelhsmissias@gmail.com>
2025-08-20 11:56:01 -0300

    refactor: add typing

--- frontend/src/contexts/orders.tsx
+++ frontend/src/contexts/orders.tsx
@@ -2,10 +2,44 @@ import { createContext, useEffect, useState, useContext } from "react";
 import { api } from "../api";
 import { DateContext } from "./date";
 
-export const OrdersContext = createContext({});
+export interface ICustomer {
+    id: number;
+    name: string;
+}
 
-const OrdersProvider = ({ children }) => {
-    const [ordersList, setOrdersList] = useState();
+interface ITask {
+    id: number;
+    name: string;
+    value: string;
+}
+
+interface IItem {
+    id: number;
+    task: ITask;
+}
+
+export interface IOrder {
+    id: number;
+    teeths: string;
+    color: string;
+    date: string;
+    customer: ICustomer;
+    items: IItem[];
+}
+
+export interface IOrdersProviderProps {
+    children: React.ReactNode;
+}
+
+interface IOrdersContext {
+    ordersList: IOrder[];
+    setOrdersList: React.Dispatch<React.SetStateAction<IOrder[] | []>>;
+}
+
+export const OrdersContext = createContext({} as IOrdersContext);
+
+const OrdersProvider = ({ children }: IOrdersProviderProps) => {
+    const [ordersList, setOrdersList] = useState<IOrder[] | []>([]);
     const { initialDate, finalDate } = useContext(DateContext);
 
     useEffect(() => {
