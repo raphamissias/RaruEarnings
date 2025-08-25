b86798d4031ae2fb0800a77ead6c580f8058bb45
Raphael <raphaelhsmissias@gmail.com>
2025-08-25 17:57:48 -0300

    feat: add transactions context

---
+++ frontend/src/contexts/transactions.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { read } from "../database/transactions";
+import { DateContext } from "./date";
+import type { ITransaction } from "../database/transactions";
+
+interface ITransactionsProviderProps {
+    children: React.ReactNode;
+}
+
+interface ITransactionsContext {
+    transactionsList: ITransaction[];
+    setTransactionsList: React.Dispatch<React.SetStateAction<ITransaction[]>>
+}
+
+export const TransactionsContext = createContext({} as ITransactionsContext);
+
+const TransactionsProvider = ({ children }: ITransactionsProviderProps) => {
+    const [transactionsList, setTransactionsList] = useState<ITransaction[] | []>([]);
+    const { initialDate, finalDate } = useContext(DateContext);
+
+    useEffect(() => {
+        const readTransactions = async () => {
+            const transactions = await read(initialDate, finalDate);
+
+            setTransactionsList(transactions);
+        }
+
+        readTransactions();
+    }, [initialDate, finalDate])
+
+    return (
+        <TransactionsContext.Provider value={{ transactionsList, setTransactionsList }}>
+            { children }
+        </TransactionsContext.Provider>
+    )
+};
+
+export default TransactionsProvider;
