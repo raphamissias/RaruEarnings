0cdcf212523e119ff4e4a58ae913f1406ef149d1
Raphael <raphaelhsmissias@gmail.com>
2025-08-28 11:17:57 -0300

    feat: add customers loading data function

--- frontend/src/components/Orders/Order/OrderInfo/index.tsx
+++ frontend/src/components/Orders/Order/OrderInfo/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import style from "./style.module.css"
+import style from "./style.module.css";
+import { readCustomer, type ICustomer } from "../../../../database/customers";
 
 interface IOrderInfoProps {
     lblText: string;
@@ -7,21 +8,42 @@ interface IOrderInfoProps {
     input?: string;
     icon?: string;
     placeholder?: string;
+    select?: string;
 }
 
-const OrderInfo = ({lblText, value, input, icon, placeholder}: IOrderInfoProps) => {
+const OrderInfo = ({lblText, value, input, icon, placeholder, select}: IOrderInfoProps) => {
     const [textField, setTextField] = useState(input);
+    const [customers, setCustomers] = useState<ICustomer[]>([]);
 
     useEffect(() => {
         setTextField(input);
     }, [input])
+
+    const getCustomers = async () => {
+        const customersArr = await readCustomer();
+
+        setCustomers(customersArr);
+    }
+
+    useEffect(() => {
+        getCustomers()
+    }, [customers])
+
 
     return (
         <div className={style.orderInfo}>
             {icon? <span className={style.icon}>{icon}</span> 
                 : <label htmlFor="">{lblText}:</label>
             }
-            {input ? <input type="text" value={textField} placeholder={placeholder} onChange={e => setTextField(e.target.value)}/> : <p>{value}</p> }
+            {input ? 
+                <input type="text" value={textField} placeholder={placeholder} onChange={e => setTextField(e.target.value)}/>
+            : select ? 
+                <select name="" id="">
+                    {customers.map(item => (
+                        <option value={item.id}>{item.name}</option>
+                    ))}
+                </select>
+            : <p>{value}</p> }
         </div>
     )
 }
