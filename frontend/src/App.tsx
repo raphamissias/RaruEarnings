dd710cd20a57e87b3847a897e66b83b6ee3f7533
Raphael <raphaelhsmissias@gmail.com>
2025-08-29 13:53:30 -0300

    chore: change import of Google icons to base index.html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import Date from "./components/Date";
 import Transactions from './components/Transactions'
 import History from './components/History'
 import Orders from "./components/Orders";
+import Nav from "./components/Nav";
 import './App.css'
 
 function App() {
 
   return (
     <>
+      <Nav></Nav>
       <Date></Date>
       <Transactions></Transactions>
       <Orders></Orders>
