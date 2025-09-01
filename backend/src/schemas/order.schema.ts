b2763ef822e9c4b0f2cd71d0ce09ac004489c408
Raphael <raphaelhsmissias@gmail.com>
2025-09-01 19:56:28 -0300

    fix: day value correction

--- backend/src/schemas/order.schema.ts
+++ backend/src/schemas/order.schema.ts
@@ -19,7 +19,7 @@ const OrderReadSchema = z.array(z.object({
     color: z.string(),
     date: z.string().transform((val) => {
       const [year, month, day] = val.split('-');
-      const date = new Date(Date.UTC(Number(year), Number(month) - 1, Number(day) + 2));
+      const date = new Date(Date.UTC(Number(year), Number(month) - 1, Number(day)));
       return date.toLocaleDateString('pt-BR');
     }),
     customer: CustomerSchema,
