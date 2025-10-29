9cf6f98a94e647ae9facc5401e007e70a20e025c
Raphael <raphaelhsmissias@gmail.com>
2025-10-29 12:27:00 -0300

    feat: add providers of customers and tasks context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,19 +4,25 @@ import { BrowserRouter } from 'react-router-dom'
 import OrdersProvider from './contexts/orders.tsx'
 import DateProvider from './contexts/date.tsx'
 import TransactionsProvider from './contexts/transactions.tsx'
+import { CustomersProvider } from './contexts/customers.tsx'
 import './index.css'
 import App from './App.tsx'
+import { TasksProvider } from './contexts/tasks.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <DateProvider>
-      <OrdersProvider>
-        <TransactionsProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </TransactionsProvider>
-      </OrdersProvider>
+      <CustomersProvider>
+        <TasksProvider>
+          <OrdersProvider>
+            <TransactionsProvider>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </TransactionsProvider>
+          </OrdersProvider>
+        </TasksProvider>
+      </CustomersProvider>
     </DateProvider>
   </StrictMode>
 )
