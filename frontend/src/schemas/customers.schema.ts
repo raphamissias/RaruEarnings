380dc5cdd6d4d53fe36e8363219dc5cfe8f76516
Raphael <raphaelhsmissias@gmail.com>
2025-10-29 12:43:36 -0300

    feat: add contact field to customer schema

--- frontend/src/schemas/customers.schema.ts
+++ frontend/src/schemas/customers.schema.ts
@@ -2,4 +2,5 @@ import z from "zod";
 
 export const customerSchema = z.object({
     name: z.string("O campo nome é obrigatório").min(3, "O nome deve ter no mínimo 3 caracteres"),
+    contact: z.string(),
 });
