32e285f029eef92893b9718cdf413f2eda56135a
Raphael <raphaelhsmissias@gmail.com>
2025-11-10 10:09:33 -0300

    fix: add decimal place to history item value view

--- frontend/src/components/History/HistoryItem/index.tsx
+++ frontend/src/components/History/HistoryItem/index.tsx
@@ -24,7 +24,7 @@ const HistoryItem = ({mode, transaction}: IHistoryItemProps) => {
                         <p className={style.date}>{ transaction.date }</p>
                     </div>
                     <div>
-                        <p className={transaction.isDiscount? style.debit : style.value}>{ transaction.isDiscount? "-" + transaction.value : "+" + transaction.value }</p>
+                        <p className={transaction.isDiscount? style.debit : style.value}>{ transaction.isDiscount? "-" + Number(transaction.value).toFixed(2) : "+" + Number(transaction.value).toFixed(2) }</p>
                     </div>
                 </li>
                 : itemMode == "update" ? 
